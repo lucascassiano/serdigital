b9b62ed29a8b650be4837b5f93bd8648bad097d7
Lucas Cassiano Silva <lucascassiano@users.noreply.github.com>
2018-10-11 01:46:19 -0300

    nightly build

--- main/components/Header.js
+++ main/components/Header.js
@@ -1,10 +1,34 @@
-import React, { Components } from "react";
+import React, { Component } from "react";
 
-export default class Header extends Components {
+import { Grid, Paper, Card } from "@material-ui/core";
+
+export default class Header extends Component {
 
     render() {
         return <div>
-            header
+            <Grid container spacing={24}>
+                <Grid item xs={12}>
+                    <Paper className="card">xs=12</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className="card">xs=6</Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className="card">xs=6</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className="card">xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className="card">xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className="card">xs=3</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className="card">xs=3</Paper>
+                </Grid>
+            </Grid>
         </div>
     }
 }
--- main/index.js
+++ main/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 
+import { Grid, Paper } from "@material-ui/core";
+
 class App extends React.Component {
     render() {
         return <div>
