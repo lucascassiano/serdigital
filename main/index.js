44858cfc406e49327de7a6b965255f3881c8810c
Lucas Cassiano Silva <lucascassiano@users.noreply.github.com>
2018-10-11 02:06:42 -0300

    test build -- git errors

--- main/index.js
+++ main/index.js
@@ -2,35 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 
-import { Grid, Paper } from "@material-ui/core";
 
 class App extends React.Component {
     render() {
         return <div>
-            <Grid container spacing={24}>
-                <Grid item xs={12}>
-                    <Header />
-                </Grid>
-
-                <Grid item xs={6}>
-                    <Paper className="card">xs=6</Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper className="card">xs=6</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className="card">xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className="card">xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className="card">xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className="card">xs=3</Paper>
-                </Grid>
-            </Grid>
+          INIT
 
         </div>
     }
